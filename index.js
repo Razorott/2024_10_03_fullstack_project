71a8959a8bd7fa32e206a10ddfec3022a7f3a7ff
PEDRO HENRICKE RODRIGUES CARDOSO <24.00818-4@maua.br>
2024-10-17 12:54:14 -0300

    Banco de dados funcionando

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-//get http://localhost:3000/filmes
-app.get("/oi", (req, res) => {
-    res.send("oi")
-})
+const Filme = mongoose.model ("Filme", mongoose.Schema({
+    titulo: {type: String},
+    sinopse: {type: String}
+}))
 
-let filmes = [
-    {
-        titulo: "A Volta dos Que Não Foram",
-        sinopse: "É um filme que trata sobre escolhas, na qual toda escolha haverá uma consequencia onde vc pode ir mesmo qeu não tenha escolhido ou não volta mais no tempo"
-    },
-    {
-        titulo: "Star Wars: Episódio III – A Vingança dos Sith",
-        sinopse: "As Guerras Clônicas estão em pleno andamento e Anakin Skywalker mantém um elo de lealdade com Palpatine, ao mesmo tempo em que luta para que seu casamento com Padmé Amidala não seja afetado por esta situação. Seduzido por promessas de poder, Anakin se aproxima cada vez mais de Darth Sidious até se tornar o temível Darth Vader. Juntos eles tramam um plano para aniquilar de uma vez por todas com os cavaleiros jedi."
-    }
-]
+async function conectarAoMongoDB() {
+    await mongoose.connect(`mongodb+srv://BancoBanco:1234@cluster0.ho9y3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+}
 
-app.get('/filmes', (req,res) => {
+// let filmes = [
+//     {
+//         titulo: "A Volta dos Que Não Foram",
+//         sinopse: "É um filme que trata sobre escolhas, na qual toda escolha haverá uma consequencia onde vc pode ir mesmo qeu não tenha escolhido ou não volta mais no tempo"
+//     },
+//     {
+//         titulo: "Star Wars: Episódio III – A Vingança dos Sith",
+//         sinopse: "As Guerras Clônicas estão em pleno andamento e Anakin Skywalker mantém um elo de lealdade com Palpatine, ao mesmo tempo em que luta para que seu casamento com Padmé Amidala não seja afetado por esta situação. Seduzido por promessas de poder, Anakin se aproxima cada vez mais de Darth Sidious até se tornar o temível Darth Vader. Juntos eles tramam um plano para aniquilar de uma vez por todas com os cavaleiros jedi."
+//     }
+// ]
+
+app.get('/filmes', async (req,res) => {
+    const filmes = await Filme.find()
+
     res.json(filmes)
 })
 
-app.post('/filmes', (req, res) => {
+app.post('/filmes', async (req, res) => {
     //capturar as infos enviadas
     const titulo =  req.body.titulo
     const sinopse = req.body.sinopse
 
-    //montar um objeto filme com as infos capturadas
-    const novo_filme = {titulo: titulo, sinopse: sinopse}
-    filmes.push(novo_filme)
+    const filme = new Filme({titulo: titulo, sinopse: sinopse})
+    await filme.save()
+
+    const filmes = await Filme.find()
     res.json(filmes)
 })
 
-app.listen (3000, () => console.log("server up & running"))
+app.listen (3000, () => {
+    try{
+        conectarAoMongoDB()
+        console.log("server up and running")
+    }
+    catch (e){
+        console.log("erro de conexão", e)
+    }
+})
